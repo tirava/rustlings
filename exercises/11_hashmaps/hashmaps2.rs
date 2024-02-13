e3ccedc22e21c328c49f46c25fbab9969479562b
Eugene Klimov <tirava@gmail.com>
2024-02-13 19:42:20 +0000

    maps done

--- exercises/11_hashmaps/hashmaps2.rs
+++ exercises/11_hashmaps/hashmaps2.rs
@@ -14,8 +14,6 @@
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 #[derive(Hash, PartialEq, Eq)]
@@ -40,6 +38,11 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
+        basket.insert(Fruit::Apple, 4);
+        basket.insert(Fruit::Mango, 2);
+        basket.insert(Fruit::Lychee, 5);
+        basket.insert(Fruit::Banana, 1);
+        basket.insert(Fruit::Pineapple, 3);
     }
 }
 
--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
@@ -39,6 +37,26 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded by team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        let team1 = Team{
+            goals_scored: team_1_score,
+            goals_conceded: team_2_score
+        };
+        let team2 = Team{
+            goals_scored: team_2_score,
+            goals_conceded: team_1_score
+        };
+        if !scores.contains_key(&team_1_name) {
+            scores.insert(team_1_name, team1);
+        } else {            
+             scores.entry(team_1_name.clone()).and_modify(|t| t.goals_scored += team_2_score);
+             scores.entry(team_1_name).and_modify(|t| t.goals_conceded += team_1_score);
+        };
+        if !scores.contains_key(&team_2_name) {
+            scores.insert(team_2_name, team2);
+        } else {            
+             scores.entry(team_2_name.clone()).and_modify(|t| t.goals_scored += team_2_score);
+             scores.entry(team_2_name).and_modify(|t| t.goals_conceded += team_1_score);
+        };
     }
     scores
 }
